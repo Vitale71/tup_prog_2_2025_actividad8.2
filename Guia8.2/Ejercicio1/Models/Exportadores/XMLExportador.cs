947f9b7d8ac25b7c2aca3e229087c04d9de270da
Vitale71 <lucasvitale71@gmail.com>
2025-10-09 17:59:01 -0300

    .

---
+++ Guia8.2/Ejercicio1/Models/Exportadores/XMLExportador.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ejercicio1.Models.Exportadores;
+
+public class XMLExportador : IExportador
+{
+    public string Exportar(Multa m)
+    {
+      
+    }
+
+    public bool Importar(string data, Multa m)
+    {
+        Regex R = new Regex(@"<patente>([A-Z]{3}[0-9]{3}</patente><vencimiento>([1-31]{2}/[1-12]{2}/[0-9]{4})</vencimiento><importe>(\d+,\d+)</importe>");
+        Match mat = R.Match(data);
+        if (mat.Success)
+        {
+            string patente = mat.Groups[1].Value;
+            DateOnly vencimiento = DateOnly.ParseExact(mat.Groups[2].Value,"dd/MM/yyyy");
+            double importe = Convert.ToDouble(mat.Groups[3].Value);
+
+            m.Patente = patente;
+            return true;
+        }
+        return false;
+    }
+}
